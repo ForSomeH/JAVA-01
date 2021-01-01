bcf8e48adc55ef3f84d0fda2d85ff30477281c32
512964300@qq.com <3618901hzw>
2021-01-01 23:48:06 +0800

    开学前的预习作业

---
+++ Week_00/src/com/sun/MyHelloWorld.java
@@ -0,0 +1,34 @@
+package com.sun;
+
+/**
+ * @author hongzhengwei
+ * @desc 作业1：分析字节码作业
+ * @date： 2020/11/21
+ */
+public class MyHelloWorld {
+	static {
+		System.out.println("show test!");
+	}
+	public void test(){
+		System.out.println("洪政伟太帅了！");
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello Class Initialized!");
+		int end = 5;
+		int result = 0;
+		for (int i = 0; i < end; i++) {
+			if (i == 0) {
+				result = result + 3;
+			}
+			if (i == 1) {
+				result = result - 1;
+			}
+			if (i == 2) {
+				result = result * 4;
+			}
+			if (i == 3) {
+				result = result / 2;
+			}
+		}
+	}
+}
