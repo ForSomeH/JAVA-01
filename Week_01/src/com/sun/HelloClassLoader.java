51b7f05278563a14c24388324e0242c9a710df2d
512964300@qq.com <3618901hzw>
2021-01-07 22:08:33 +0800

    作业1的优化

--- Week_01/src/com/sun/HelloClassLoader.java
+++ Week_01/src/com/sun/HelloClassLoader.java
@@ -1,5 +1,6 @@
 package com.sun;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URI;
 import java.nio.file.Files;
@@ -16,12 +17,19 @@ public class HelloClassLoader extends ClassLoader {
     public static void main(String[] args) {
         try {
             // 加载并初
-            new HelloClassLoader().findClass("com.sun.Hello").newInstance();
+//            new HelloClassLoader().findClass("Hello").newInstance();
+            Class<?> clazz = new HelloClassLoader().findClass("Hello");
+            Method declaredMethod = clazz.getDeclaredMethod("hello");
+            declaredMethod.invoke(clazz.newInstance());
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
 
@@ -30,66 +38,45 @@ public class HelloClassLoader extends ClassLoader {
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         //通过类文件获取字节码
-		String myPath = "file:///Users/summer/IdeaProjects/JAVA-01/out/production/JAVA-01/com/sun/Hello.class";
-		byte[] cLassBytes = null;
-		Path path = null;
-		try {
-			path = Paths.get(new URI(myPath));
-			cLassBytes = Files.readAllBytes(path);
+//		String myPath = "file:///Users/summer/IdeaProjects/JAVA-01/out/production/JAVA-01/com/sun/Hello.class";
+        String myPath = "file:///Users/summer/IdeaProjects/JAVA-01/Week_01/Hello.xlass";
+
+        byte[] cLassBytes = null;
+        Path path = null;
+        try {
+            path = Paths.get(new URI(myPath));
+            cLassBytes = Files.readAllBytes(path);
             for (byte cLassByte : cLassBytes) {
 //                byte i = 255 - cLassByte;
             }
-			//将字节转换为int计算
+            //将字节转换为int计算
 //			int old = byteArrayToInt(cLassBytes);
 //			old = 255 - old;
 //			cLassBytes = intToByteArray(old);
-			Class clazz = defineClass(name, cLassBytes, 0, cLassBytes.length);
-			Object obj = clazz.newInstance();
-			Method method = clazz.getMethod("test");
-			method.invoke(obj);
-			return clazz;
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+            for (int i = 0; i < cLassBytes.length; i++) {
+                cLassBytes[i] = (byte) (255 - cLassBytes[i]);
+            }
+            Class clazz = defineClass(name, cLassBytes, 0, cLassBytes.length);
+            Object obj = clazz.newInstance();
+            Method method = clazz.getMethod("hello");
+            method.invoke(obj);
+            return clazz;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         //通过加密获取字节码
 //        String helloBase64 = "yv66vgAAADQAHwoABgARCQASABMIABQKABUAFgcAFwchbFZhcmlhYmxlVGFibGUBAAR0aGlzAQALTGp2bS9IZWxsbzsBAAg8Y2xGxvIENsYXNzIEluaXRpYWxpemVkIQcAHAwAHQAeAQAJanZtL0hlbGxvA YS9pby9QcmludFN0cmVhbTsBABNqYXZhL2lvL1ByaW50U3RyZWFtAQAH ABAAkAAAAvAAEAAQAAAAUqtwABsQAAAAIACgAAAAYAAQAAAAMACwAAAA AAAACgACAAAABgAIAAcAAQAPAAAAAgAQ";
 //        byte[] bytes = decode(helloBase64);
 //        return defineClass(name, bytes, 0, bytes.length);
-		return null;
+        return null;
     }
 
-    public byte[] decode(String base64) {
+    public byte[] decodeByBase64(String base64) {
+        return Base64.getDecoder().decode(base64);
+    }
+    public byte[] decodeBy255(String base64) {
         return Base64.getDecoder().decode(base64);
     }
 
 
-    /**
-     * int到byte[] 由高位到低位
-     *
-     * @param i 需要转换为byte数组的整行值。
-     * @return byte数组
-     */
-    public static byte[] intToByteArray(int i) {
-        byte[] result = new byte[4];
-        result[0] = (byte) ((i >> 24) & 0xFF);
-        result[1] = (byte) ((i >> 16) & 0xFF);
-        result[2] = (byte) ((i >> 8) & 0xFF);
-        result[3] = (byte) (i & 0xFF);
-        return result;
-    }
-
-    /**
-     * byte[]转int
-     *
-     * @param bytes 需要转换成int的数组
-     * @return int值
-     */
-    public static int byteArrayToInt(byte[] bytes) {
-        int value = 0;
-        for (int i = 0; i < 4; i++) {
-            int shift = (3 - i) * 8;
-            value += (bytes[i] & 0xFF) << shift;
-        }
-        return value;
-    }
 }
