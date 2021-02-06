b65bcf3e6aa3bfde38fb206e6172a2ec6afae4f6
hongzhengwei <3618901hzW>
2021-02-06 21:25:29 +0800

    作业1，2，3提交

--- Week_05/src/main/java/com/summer/SpringTestApplication.java
+++ Week_05/src/main/java/com/summer/SpringTestApplication.java
@@ -2,7 +2,6 @@ package com.summer;
 
 
 import com.summer.beans.Company;
-import com.summer.beans.Department;
 import com.summer.beans.User;
 import com.summer.beans.UserC;
 import com.summer.business.Dinner;
@@ -23,6 +22,10 @@ public class SpringTestApplication {
         //todo 作业3xml装配一组bean
         homeWork3(context);
         //todo 作业4将netty的inbound，outbound，filter，router交给spring管理
+        //todo   4.2 (挑战)基于 AOP 改造 Netty 网关，filter 和 router 使用 AOP 方式实现;
+        //todo   4.3 (中级挑战)基于前述改造，将网关请求前后端分离，中级使用 JMS 传递消息;
+        //todo   4.4 (中级挑战)尝试使用 ByteBuddy 实现一个简单的基于类的 AOP;
+        //todo   4.5 (超级挑战)尝试使用 ByteBuddy 与 Instrument 实现一个简单 JavaAgent 实现无侵入 下的 AOP。
         //netty改造
         //初始化后端服务请求的数据
         String proxyServers = System.getProperty("proxyServers", "http://localhost:8088/api/hello");
@@ -37,41 +40,41 @@ public class SpringTestApplication {
     }
 
     /**
-     * @author  hongzhengwei
-     * @create  2021/2/5 9:07 下午
-     * @desc    作业1，aop动态代理
+     * @author hongzhengwei
+     * @create 2021/2/5 9:07 下午
+     * @desc 作业1，aop动态代理
      **/
-    static void homeWork1(ApplicationContext context){
+    static void homeWork1(ApplicationContext context) {
         Dinner dinner = context.getBean(Dinner.class);
         System.out.println("Dinner对象AOP代理后的实际类型是否是Dinner子类：" + (dinner instanceof Dinner));
         dinner.eating();
     }
 
     /**
-     * @author  hongzhengwei
-     * @create  2021/2/5 9:07 下午
-     * @desc    作业2不同方式装配bean
+     * @author hongzhengwei
+     * @create 2021/2/5 9:07 下午
+     * @desc 作业2不同方式装配bean
      **/
-    static void homeWork2(ApplicationContext context){
+    static void homeWork2(ApplicationContext context) {
         //方式1：xml配置接口
         ISleep ISleep = context.getBean(ISleep.class);
         System.out.println(ISleep);
         System.out.println("第一种加载方式，xml配置，接口的对象AOP代理后的实际类型：" + ISleep.getClass());
         //方式2：@Bean注解bean
-        User user= (User) context.getBean("beanUser");
-        System.out.println("第二种加载方式，@Bean获取：" +user);
+        User user = (User) context.getBean("beanUser");
+        System.out.println("第二种加载方式，@Bean获取：" + user);
         //方式3：@Component
-        UserC userC=context.getBean(UserC.class);
-        System.out.println("第三种加载方式，@Bean获取：" +userC);
+        UserC userC = context.getBean(UserC.class);
+        System.out.println("第三种加载方式，@Bean获取：" + userC);
     }
 
     /**
-     * @author  hongzhengwei
-     * @create  2021/2/5 9:07 下午
-     * @desc    作业3，xml方式装配一组bean
+     * @author hongzhengwei
+     * @create 2021/2/5 9:07 下午
+     * @desc 作业3，xml方式装配一组bean
      * xml装配bean Company->Department->User,其中company是注解，其余是xml指定
      **/
-    static void homeWork3(ApplicationContext context){
+    static void homeWork3(ApplicationContext context) {
         Company gillion = context.getBean(Company.class);
         System.out.println(gillion);
     }
