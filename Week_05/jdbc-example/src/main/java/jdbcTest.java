92447e205a7f6815b22495f402ca52f909998d0b
hongzhengwei <3618901hzW>
2021-02-19 10:23:48 +0800

    批处理

--- Week_05/jdbc-example/src/main/java/jdbcTest.java
+++ Week_05/jdbc-example/src/main/java/jdbcTest.java
@@ -14,8 +14,8 @@ import java.util.List;
 public class jdbcTest {
     public static void main(String[] args) throws SQLException {
         List<User> result = new ArrayList<>(10);
-        User user = new User("exception", 12, 18);
-        User user2 = new User("exception", 11, 19);
+        User user = new User("exception", 14, 18);
+        User user2 = new User("exception", 15, 19);
         UserService userService = new UserServiceImpl();
         String savaSql = userService.save(user);
         String savaSql2 = userService.save(user2);
@@ -26,7 +26,7 @@ public class jdbcTest {
         DateSource dateSource = null;
         try {
             dateSource = new DateSource();
-            dateSource.getConnection();
+            Connection connection = dateSource.getConnection();
             //创建一个preparedStatement
             Statement stmt = dateSource.getStatement();
             int flag1 = stmt.executeUpdate(savaSql);
@@ -43,6 +43,22 @@ public class jdbcTest {
                 result.add(resultUser);
             }
             stmt.executeLargeUpdate(deleteSql);
+
+            //批处理
+            try (PreparedStatement ps = connection.prepareStatement("update user set age=? where id=?")) {
+                // 对同一个PreparedStatement反复设置参数并调用addBatch():
+                for (User s : result) {
+                    ps.setInt(1, s.getAge()+100);
+                    ps.setInt(2, s.getId());
+                    ps.addBatch(); // 添加到batch
+                }
+                // 执行batch:
+                int[] ns = ps.executeBatch();
+                for (int n : ns) {
+                    System.out.println(n + " update."); // batch中每个SQL执行的结果数量
+                }
+            }
+            connection.commit();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
